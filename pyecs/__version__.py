6cd5ac63606c30cd963082b053816224169c5118
tim-fi <me@timfi.dev>
2020-08-20 15:43:07 +0200

    bumbed version number

--- pyecs/__version__.py
+++ pyecs/__version__.py
@@ -1,8 +1,8 @@
 __title__ = "pyecs"
 __description__ = "A simple implementation of the Entity-Component pattern."
 __url__ = "https://github.com/tim-fi/pyecs"
-__version__ = "0.9"
-__build__ = 0x000009
+__version__ = "0.10"
+__build__ = 0x00000A
 __author__ = "Tim Fischer"
 __author_email__ = "me@timfi.dev"
 __license__ = "MIT"
