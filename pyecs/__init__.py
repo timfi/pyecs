80d967647cfdb7112d097b0d3e7bd6254c7f13a8
tim-fi <me@timfi.dev>
2020-08-25 22:59:51 +0200

    Moved to stub file for typing
    To facilitate the stubs files in the packaging
    I needed to switch to a package based distribution
    from the current module based one as PEP-561 specifcally
    states that the proper distribution of type information
    is only supported with package based distribution.

--- pyecs/__init__.py
+++ pyecs/__init__.py
@@ -1,51 +1,22 @@
 """Entity-Component Store."""
-from __future__ import annotations
-
 from collections import defaultdict
-from typing import (
-    TYPE_CHECKING,
-    Any,
-    Callable,
-    Dict,
-    Optional,
-    Set,
-    Tuple,
-    Type,
-    TypeVar,
-)
-from uuid import UUID
+from functools import lru_cache
 from uuid import uuid4 as get_uuid
 
-F = TypeVar("F", bound=Callable)
-if TYPE_CHECKING:
-
-    def lru_cache(f: F) -> F:  # noqa
-        ...
-
-
-else:
-    from functools import lru_cache
-
-
-__version__ = "0.15"
+__version__ = "0.16"
 __all__ = ("Store", "Entity")
-
-
-C = TypeVar("C")
 
 
 class Entity:
     """A wrapper around an entity-uuid and entity-component store."""
 
     __slots__ = ("_store", "_uuid")
-    _store: Store
-    _uuid: UUID
 
-    def __init__(self, store: Store, uuid: UUID):  # noqa
+    def __init__(self, store, uuid):  # noqa
         self._store = store
         self._uuid = uuid
 
-    def __repr__(self) -> str:  # noqa
+    def __repr__(self):  # noqa
         return f"Entity(uuid={self._uuid})"
 
     __str__ = __repr__
@@ -55,78 +26,77 @@ class Entity:
         """Identifier of this entity in the Store."""
         return self._uuid
 
-    def __eq__(self, other: Any) -> bool:  # noqa
+    def __eq__(self, other):  # noqa
         return isinstance(other, Entity) and self.uuid == other.uuid
 
-    def add_child(self, *components: Any, uuid: Optional[UUID] = None) -> Entity:
+    def add_child(self, *components, uuid=None):
         """Create child entity.
 
-        :param *components: Any: Components to add to the entity.
-        :param uuid: Optional[UUID]: Override UUID, if given this skips the automatic
-                                     UUID generation. (Default value = None)
+        :param *components: Components to add to the entity.
+        :param uuid: Override UUID, if given this skips the automatic
+                     UUID generation. (Default value = None)
 
         """
         return self._store.add_entity(*components, parent=self, uuid=uuid)
 
-    def add_components(self, *components: Any):
+    def add_components(self, *components):
         """Add components to entity.
 
-        :param *components: Any: Components to add to the entity.
+        :param *components: Components to add to the entity.
 
         """
         self._store.add_components(self.uuid, *components)
 
-    def get_component(self, c_type: Type[C]) -> C:
+    def get_component(self, c_type):
         """Get a single component from entity.
 
-        :param c_type: Type[C]: Type of the component to get from the entity.
+        :param c_type: Type of the component to get from the entity.
 
         """
         return self._store.get_component(self.uuid, c_type)
 
-    def get_components(self, *c_types: type) -> Tuple[Any, ...]:
+    def get_components(self, *c_types):
         """Get multiple components from entity.
 
-        Note that proper typing is only supported for the single component
-        access (get_component).
+        Note that proper typing is only ensured up to 10 components.
 
-        :param *c_types: type: Types of the components to get from the entity,
-                               the result will be in the order these types are given.
+        :param *c_types: Types of the components to get from the entity,
+                         the result will be in the order these types are given.
 
         """
         return self._store.get_components(self.uuid, *c_types)
 
-    def get_parent(self) -> Optional[Entity]:
+    def get_parent(self):
         """Get parent entity."""
         return self._store.get_parent(self.uuid)
 
-    def get_children(self) -> Tuple[Entity, ...]:
+    def get_children(self):
         """Get all child entities."""
         return self._store.get_children(self.uuid)
 
-    def get_children_with(self, *c_types: type) -> Tuple[Entity, ...]:
+    def get_children_with(self, *c_types):
         """Get all children with the given components.
 
-        :param *c_types: type: Types of the components the entities should have.
+        :param *c_types: Types of the components the entities should have.
 
         """
         return self._store.get_children_with(self.uuid, *c_types)
 
-    def remove_components(self, *c_types: type, delay: bool = False):
+    def remove_components(self, *c_types, delay=False):
         """Remove component from entity.
 
-        :param *c_types: type: Types of the components to remove from the entity.
-        :param delay: bool: Delay the removal of the components until `Store.apply_removals`
-                            is called. (Default value = False)
+        :param *c_types: Types of the components to remove from the entity.
+        :param delay: Delay the removal of the components until `Store.apply_removals`
+                      is called. (Default value = False)
 
         """
         self._store.remove_components(self.uuid, *c_types, delay=delay)
 
-    def remove(self, *, delay: bool = False):
+    def remove(self, *, delay=False):
         """Remove entity from Store.
 
-        :param delay: bool: Delay the removal of this entity until `Store.apply_removals`
-                            is called. (Default value = False)
+        :param delay: Delay the removal of this entity until `Store.apply_removals`
+                      is called. (Default value = False)
 
         """
         self._store.remove_entity(self.uuid, delay=delay)
@@ -143,13 +113,6 @@ class Store:
         "entity_delete_buffer",
         "component_delete_buffer",
     )
-
-    entities: Dict[UUID, Set[str]]
-    entity_hirarchy: Dict[UUID, Set[UUID]]
-    entity_hirarchy_rev: Dict[UUID, Optional[UUID]]
-    components: Dict[str, Dict[UUID, Any]]
-    entity_delete_buffer: Set[UUID]
-    component_delete_buffer: Set[Tuple[UUID, type]]
 
     def __init__(self):  # noqa
         self.entities = {}
@@ -177,19 +140,14 @@ class Store:
         self.entity_delete_buffer.clear()
         self.component_delete_buffer.clear()
 
-    def add_entity(
-        self,
-        *components: Any,
-        parent: Optional[Entity] = None,
-        uuid: Optional[UUID] = None,
-    ) -> Entity:
+    def add_entity(self, *components, parent=None, uuid=None):
         """Add entity to store.
 
-        :param *components: Any: Components to add to the entity.
-        :param parent: Optional[Entity]: Entity to register as a parent
-                                         entity. (Default value = None)
-        :param uuid: Optional[UUID]: Override UUID, if given this skips the automatic
-                                     UUID generation. (Default value = None)
+        :param *components: Components to add to the entity.
+        :param parent: Entity to register as a parent
+                       entity. (Default value = None)
+        :param uuid: Override UUID, if given this skips the automatic
+                     UUID generation. (Default value = None)
 
         """
         if uuid and uuid in self.entities:
@@ -207,11 +165,11 @@ class Store:
         self.add_components(uuid, *components)
         return Entity(self, uuid)
 
-    def add_components(self, uuid: UUID, *components: Any):
+    def add_components(self, uuid, *components):
         """Add components to an existing entity.
 
-        :param uuid: UUID: UUID of the entity to add the components to.
-        :param *components: Any: Components to add to the entity.
+        :param uuid: UUID of the entity to add the components to.
+        :param *components: Components to add to the entity.
 
         """
         for component in components:
@@ -221,25 +179,24 @@ class Store:
 
         self.clear_cache()
 
-    def get_component(self, uuid: UUID, c_type: Type[C]) -> C:
+    def get_component(self, uuid, c_type):
         """Get a single component from an entity.
 
-        :param uuid: UUID: UUID of the entity to get the component from.
-        :param c_type: Type[C]: Type of the component to get from the entity.
+        :param uuid: UUID of the entity to get the component from.
+        :param c_type: Type of the component to get from the entity.
 
         """
-        return self.components[c_type.__qualname__][uuid]  # type: ignore
+        return self.components[c_type.__qualname__][uuid]
 
     @lru_cache
-    def get_components(self, uuid: UUID, *c_types: type) -> Tuple[Any, ...]:
+    def get_components(self, uuid, *c_types):
         """Get multiple component from an entity.
 
-        Note that proper typing is only supported for the single component
-        access (get_component).
+        Note that proper typing is only ensured up to 10 components.
 
-        :param uuid: UUID: UUID of the entity to get the components from.
-        :param *c_types: type: Types of the components to get from the entity,
-                               the result will be in the order these types are given.
+        :param uuid: UUID of the entity to get the components from.
+        :param *c_types: Types of the components to get from the entity,
+                         the result will be in the order these types are given.
 
         """
         if c_types:
@@ -249,10 +206,10 @@ class Store:
                 self.components[c_name][uuid] for c_name in self.entities[uuid]
             )
 
-    def get_entity(self, uuid: UUID) -> Entity:
+    def get_entity(self, uuid):
         """Get an entity for a given UUID.
 
-        :param uuid: UUID: UUID of the entity to get.
+        :param uuid: UUID of the entity to get.
 
         """
         if uuid in self.entities:
@@ -260,15 +217,15 @@ class Store:
         else:
             raise KeyError("Unknown entity id.")
 
-    def get_entities(self) -> Tuple[Entity, ...]:
+    def get_entities(self):
         """Get all entities in this store."""
         return tuple(Entity(self, uuid) for uuid in self.entities)
 
     @lru_cache
-    def get_entities_with(self, *c_types: type) -> Tuple[Entity, ...]:
+    def get_entities_with(self, *c_types):
         """Get all entities with the given components.
 
-        :param *c_types: type: Types of the components the entities should have.
+        :param *c_types: sTypes of the components the entities should have.
 
         """
         target_c_names = {c_type.__qualname__ for c_type in c_types}
@@ -278,19 +235,19 @@ class Store:
             if target_c_names <= c_names
         )
 
-    def get_children(self, uuid: UUID) -> Tuple[Entity, ...]:
+    def get_children(self, uuid):
         """Get children of an entity.
 
-        :param uuid: UUID: UUID of the entity to get the children for.
+        :param uuid: UUID of the entity to get the children for.
 
         """
         return tuple(Entity(self, c_uuid) for c_uuid in self.entity_hirarchy[uuid])
 
     @lru_cache
-    def get_children_with(self, uuid: UUID, *c_types: type) -> Tuple[Entity, ...]:
+    def get_children_with(self, uuid, *c_types):
         """Get all children of an entity with the given components.
 
-        :param *c_types: type: Types of the components the entities should have.
+        :param *c_types: Types of the components the entities should have.
 
         """
         target_c_names = {c_type.__qualname__ for c_type in c_types}
@@ -300,10 +257,10 @@ class Store:
             if target_c_names <= self.entities[c_uuid]
         )
 
-    def get_parent(self, uuid: UUID) -> Optional[Entity]:
+    def get_parent(self, uuid):
         """Get parent of an entity.
 
-        :param uuid: UUID: UUID of the entity to get the parent for.
+        :param uuid: UUID of the entity to get the parent for.
 
         """
         return (
@@ -312,17 +269,17 @@ class Store:
             else None
         )
 
-    def _remove_component(self, uuid: UUID, c_type: type):  # noqa
+    def _remove_component(self, uuid, c_type):
         self.entities[uuid] -= {c_type.__qualname__}
         del self.components[c_type.__qualname__][uuid]
 
-    def remove_components(self, uuid: UUID, *c_types: type, delay: bool = False):
+    def remove_components(self, uuid, *c_types, delay=False):
         """Remove components from an entity.
 
-        :param uuid: UUID: UUID of the entity to remove the components from.
-        :param *c_types: type: Types of the components to remove from the entity.
-        :param delay: bool: Delay the removal of the components until `Store.apply_removals`
-                            is called. (Default value = False)
+        :param uuid: UUID of the entity to remove the components from.
+        :param *c_types: Types of the components to remove from the entity.
+        :param delay: Delay the removal of the components until `Store.apply_removals`
+                      is called. (Default value = False)
 
         """
         if delay:
@@ -333,7 +290,7 @@ class Store:
                 self._remove_component(uuid, c_type)
             self.clear_cache()
 
-    def _remove_entity(self, uuid: UUID):  # noqa
+    def _remove_entity(self, uuid):  # noqa
         for c_name in self.entities.pop(uuid, ()):
             del self.components[c_name][uuid]
 
@@ -345,12 +302,12 @@ class Store:
 
         del self.entity_hirarchy[uuid]
 
-    def remove_entity(self, uuid: UUID, *, delay: bool = False):
+    def remove_entity(self, uuid, *, delay=False):
         """Remove an entity.
 
-        :param uuid: UUID: UUID of the entity to remove.
-        :param delay: bool: Delay the removal of this entity until `Store.apply_removals`
-                            is called. (Default value = False)
+        :param uuid: UUID of the entity to remove.
+        :param delay: Delay the removal of this entity until `Store.apply_removals`
+                      is called. (Default value = False)
 
         """
         if delay:
